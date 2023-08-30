2a2d6e8a87fd8407fa5f953b54886c9386b7f918
Fade <lifelessbotfromdiscord@gmail.com>
2023-08-30 14:32:36 +0530

    Fixed Bug

--- Clock/script.js
+++ Clock/script.js
@@ -1,17 +1,18 @@
-let date = new Date();
-let hour = date.getHours();
-let min = date.getMinutes();
-let sec = date.getSeconds();
-let d = date.getDate();
-let day = date.getDay();
-let month = date.getMonth();
-let year = date.getFullYear();
-
 let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
 let week = ["Monday", "Tuesday", "Wednesday", "Thrusday", "Friday", "Saturday", "Sunday"]
 
 let timeUpdate = () => {
+    let date = new Date();
+
+    let hour = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+    let d = date.getDate();
+    let day = date.getDay();
+    let month = date.getMonth();
+    let year = date.getFullYear();
+
     let time = document.getElementsByClassName("time")[0];
     let datee = document.getElementsByClassName("date")[0];
 
@@ -19,4 +20,4 @@ let timeUpdate = () => {
     datee.innerHTML = `${week[day - 1]}, ${months[month]} ${d}, ${year}`;
 }
 
-setInterval(timeUpdate, 1000);
+setInterval(timeUpdate, 500);
